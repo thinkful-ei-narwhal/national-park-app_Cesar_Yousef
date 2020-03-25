985f0d3ef7338957c639aa07c1eec13101bbf149
Cesar Morales <cesarlenin2@live.com>
2020-03-25 13:32:10 -0700

    got url working for 1 state only

--- index.js
+++ index.js
@@ -2,30 +2,37 @@
 //key x3nLzhtM5eE1OkfNzSLy9o6BqMsqFOmzhHBWgKro
 // api https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=x3nLzhtM5eE1OkfNzSLy9o6BqMsqFOmzhHBWgKro
 
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
+const apiKey = 'x3nLzhtM5eE1OkfNzSLy9o6BqMsqFOmzhHBWgKro'; 
+
 function watchForm(){
     $('form').submit(event =>{
         event.preventDefault();
         const searchTerm = $('#js-search-term').val().split(' ');
+        console.log(searchTerm);
         const maxResults = $('#js-max-results').val();
-        getYouTubeVideos(searchTerm, maxResults);
+        getParks(searchTerm, maxResults);
     })
 }
 
-function getYouTubeVideos(searchTerm, maxResults) {
+function getParks(searchTerm, maxResults) {
     const params = {
-        key: 'x3nLzhtM5eE1OkfNzSLy9o6BqMsqFOmzhHBWgKro',
-        q: searchTerm,
-        part: 'snippet',
-        maxResults,
-        type: 'video'
+        stateCode: searchTerm,
+        limit:maxResults,
+        api_key: apiKey
       };
-
       const queryString = formatQueryParams(params)
       const url = searchURL + '?' + queryString;
       
       console.log(url);
-      //"https://api.nps.gov/api/v1/parks?stateCode=ca&stateCode=az&limit=10" -H  "accept: application/json"
+      //"https://api.nps.gov/api/v1/parks?stateCode=ca&stateCode=az&limit=10" 
 }
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+  }
 
 
 $(watchForm);
