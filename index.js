8e251e7555772bc7769aba74370b149bc50820e9
Cesar Morales <cesarlenin2@live.com>
2020-03-25 13:11:44 -0700

    working on setting up the url

--- index.js
+++ index.js
@@ -6,10 +6,25 @@ function watchForm(){
     $('form').submit(event =>{
         event.preventDefault();
         const searchTerm = $('#js-search-term').val().split(' ');
-        console.log(searchTerm);
         const maxResults = $('#js-max-results').val();
-        console.log(maxResults);
+        getYouTubeVideos(searchTerm, maxResults);
     })
+}
+
+function getYouTubeVideos(searchTerm, maxResults) {
+    const params = {
+        key: 'x3nLzhtM5eE1OkfNzSLy9o6BqMsqFOmzhHBWgKro',
+        q: searchTerm,
+        part: 'snippet',
+        maxResults,
+        type: 'video'
+      };
+
+      const queryString = formatQueryParams(params)
+      const url = searchURL + '?' + queryString;
+      
+      console.log(url);
+      //"https://api.nps.gov/api/v1/parks?stateCode=ca&stateCode=az&limit=10" -H  "accept: application/json"
 }
 
 
